d1ae45a903aa4493d736d3bc0f9c152b773e89fb
JemielynM <jemielyn.bingham@triosstudent.com>
2026-05-23 22:14:03 -0400

    Initial Commit

---
+++ iOSApp1/iOSApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp1App.swift
+//  iOSApp1
+//
+//  Created by Jemielyn Medina on 2026-05-23.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSApp1Tests/iOSApp1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp1Tests.swift
+//  iOSApp1Tests
+//
+//  Created by Jemielyn Medina on 2026-05-23.
+//
+
+import Testing
+@testable import iOSApp1
+
+struct iOSApp1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
